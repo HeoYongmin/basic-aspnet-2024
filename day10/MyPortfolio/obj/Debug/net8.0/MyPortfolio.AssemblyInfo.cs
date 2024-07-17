8da0512ec2d8184c679005e7964e5ba402c9a08f
HeoYongmin <snrn2634@naver.com>
2024-07-17 17:12:51 +0900

    포트폴리오 페이지 수정

--- day10/MyPortfolio/obj/Debug/net8.0/MyPortfolio.AssemblyInfo.cs
+++ day10/MyPortfolio/obj/Debug/net8.0/MyPortfolio.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MyPortfolio")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4bbba57c8c40d6a56a76d3fbca22b2e0e0ce2eab")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4a19b6b18006589cd3acd9391ea76e1b13af1b38")]
 [assembly: System.Reflection.AssemblyProductAttribute("MyPortfolio")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MyPortfolio")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
